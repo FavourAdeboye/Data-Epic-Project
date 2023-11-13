926144679470f3ec8d4a10299d701a80eb38955d
Favour Adeboye <149948883+FavourAdeboye@users.noreply.github.com>
2023-11-13 21:41:02 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# This is a welcome message
+greeting = 'Hello'
+name = 'Data Epic Mentors'
+intro = f'{greeting}, {name[:10].lower()} {name[10:].upper()}. Welcome!'
+print(intro)
+follow_up_message = "This is Favour following a video on how to use VSCode"
+print(follow_up_message)
+print(len(follow_up_message))
+print(follow_up_message[0:14])
+message = """Tom and jerry was a fun cartoon
+I watched while growing up"""
+print(message)
+print(message[:31].lower())
+print(message[31:].upper())
+more = '''I loved it so much
+I still watch it
+when I'm home alone'''
+print(more)
+print(len(more))
+print(more[0:18])
---
+++ test.py
@@ -0,0 +1,30 @@
+print("Test file")
+num = 3
+num += 1
+number = 3.14
+print(type(num))
+print(type(number))
+print(num)
+print(number)
+# Arithimetic Operations:               # Comparison Operators:
+# Addition: num + number                # Equal: num == number
+# Substraction: number - num            # Not Equal: num != number
+# Multiplication: num * number          # Greater Than: num > number
+# Division: num / number                # Less Than: num < number
+# Floor Division: num // number         # Greater or Equal: num >= number
+# Exponent: num ** number               # Less or Equal: num <= number
+# Mobulus: num % number
+print(num == number)
+print(num != number)
+print(num > number)
+print(num < number)
+print(num >= number)
+print(num <= number)
+print(round(num + number, 2))
+print(round(abs(number - num)))
+print(num * number)
+print(round(num / number, 4))
+print(round(num // number))
+print(round(num ** number, 4))
+print(num % number)
+print(round(num * (number + 2), 2))
